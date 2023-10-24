1e1fa012d9fb3369b22bf28e76b11afe5e78f9da
cauasauer <128698814+cauasauer@users.noreply.github.com>
2023-10-24 15:51:01 -0300

    materias

---
+++ atividade_1.php
@@ -0,0 +1,25 @@
+<?php
+
+$lista = array(
+    array("banco de dados", 7.0),
+    array("análise", 6.8),
+    array("ciencia da computação", 6.3),
+    array("front end", 8.9),
+    array("mobile", 7.3),
+    array("portugues", 9.0),
+    array("matematica", 6.0),
+    array("ingles", 8.7),
+    array("historia", 7.2),
+    array("geografia", 8.0),
+    array("quimica", 10.0),
+    array("sociologia", 7.2),
+    array("projeto de vida", 10.0),
+    array("financeira", 10.0),
+    array("ingles", 8.9),
+);
+echo "<br/><br/><br/><br/>";
+foreach($lista as $posicao => $item){
+    echo $materia[0]."<br/><br/>";
+}
+
+?>
