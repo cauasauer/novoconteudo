d2569aefcb86d61647d1b9be3b2062b1122e7a09
cauasauer <128698814+cauasauer@users.noreply.github.com>
2023-11-21 16:03:17 -0300

    banco de dados

--- aula21/formulario_fibonacci.php
+++ aula21/formulario_fibonacci.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Fibonacci Calcular</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Fibonacci Calculator</title>
 </head>
 <body>
 <STYle>
@@ -47,11 +46,11 @@ li {
   margin-bottom: 10px;
 }
 </STYle>
-<h2>Fibonacci Calcular</h2>
+<h2>Fibonacci Calculator</h2>
 
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-Insira o valor da sequência de Fibonacci: <input type="number" name="num" min="1" value="<?php echo $num; ?>">
-    <input type="submit" name="submit" value="Enviar">
+    Enter the nth value of the Fibonacci sequence: <input type="number" name="num" min="1" value="<?php echo $num; ?>">
+    <input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
@@ -77,6 +76,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
 </body>
 </html>
